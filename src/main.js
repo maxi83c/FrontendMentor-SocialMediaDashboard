7d51270f4b0eabb6cb61ff17b7e2f1ee6cb1dda8
misterx83c <53312260+misterx83c@users.noreply.github.com>
2020-09-19 14:43:58 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,148 +14,58 @@ const twitterLikes = document.getElementById('twitter-likes')
 const youtubeLikes = document.getElementById('youtube-likes')
 const youtubeViews = document.getElementById('youtube-views')
 
-// Lista de objetos para simular distintos datos
-const cards =[
-  {
-    'total-followers' : '23,004',
-    'facebook-account' : '@nathanf',
-    'facebook-followers' : '1987',
-    'facebook-today' : '+12',
-    'facebook-pages' : ['87', '+3%'], 
-    'facebook-likes' : ['52', '-2%'], 
-    'twitter-account' : '@nathanf',
-    'twitter-followers' : '1044',
-    'twitter-today' : '+99',
-    'twitter-retweets' : ['117', '+303%'],
-    'twitter-likes' : ['507', '+553%'],
-    'instagram-account' : '@realnathanf',
-    'instagram-followers' : '11k',
-    'instagram-today' : '+1099',
-    'instagram-likes' : ['5462', '+2257%'],
-    'instagram-views' : ['52k', '+1375%'],
-    'youtube-account' : 'Nathan F.',
-    'youtube-suscribers' : '8239',
-    'youtube-today' : '-144',
-    'youtube-likes' : ['107', '-19%'],
-    'youtube-views' : ['1407', '-12%']
-  },
-  {
-    'total-followers' : '22,245,008',
-    'facebook-account' : '@yotuel',
-    'facebook-followers' : '3k',
-    'facebook-today' : '+124',
-    'facebook-pages' : ['6', '-7%'], 
-    'facebook-likes' : ['68', '+1%'], 
-    'twitter-account' : '@yotuel',
-    'twitter-followers' : '104K',
-    'twitter-today' : '+966',
-    'twitter-retweets' : ['1', '-39%'],
-    'twitter-likes' : ['68', '+32%'],
-    'instagram-account' : '@yotuel',
-    'instagram-followers' : '16M',
-    'instagram-today' : '+31099',
-    'instagram-likes' : ['54M', '+257%'],
-    'instagram-views' : ['50k', '+4375%'],
-    'youtube-account' : 'Yo Tu El',
-    'youtube-suscribers' : '20K',
-    'youtube-today' : '447',
-    'youtube-likes' : ['10K', '+19%'],
-    'youtube-views' : ['140K', '-1%']
-  }
-]
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max + - min)) + min;
+}
+
+const formatNumber = (number) => {
+  if (number > 999999)
+    return `${Math.floor(number / 1000000)}M`
+  if (number > 9999)
+    return `${Math.floor(number / 1000)}K` 
+  return `${number}`
+}
+
+// cuentas disponibles por red social
+const accounts = [['@nathanf', '@nathanf', '@realnathanf','Nathan F.'],
+['@abcde', '@abcde', '@abcde','The ABC'],
+['@jscsshtml', '@jscsshtml', '@jscsshtml','JS CSS HTML']]
+
+//
+const cards = [facebookCard, twitterCard, instagramCard, youtubeCard]
+const overviews = [facebookPages, facebookLikes, instagramLikes, instagramViews, twitterRetweets, twitterLikes, youtubeLikes, youtubeViews]
 
 //Función para carga de datos del dashboard
 const setData = (socialObjet) => {
 
-  totalFollowers.textContent = `Total Followers:  ${socialObjet['total-followers']}`
-
-  facebookCard.children[0].textContent = socialObjet['facebook-account']
-  facebookCard.children[1].textContent = socialObjet['facebook-followers']
-  facebookCard.children[3].textContent = `${socialObjet['facebook-today'].replace('+','')} Today`
-  if (socialObjet['facebook-today'].charAt(0) === '+')
-      facebookCard.children[3].classList.add('green')
+  
+  
+  let followers = 0;
+  
+  const selectedAccounts = getRandomInt(0, 3)
+  
+  cards.forEach((card, index) =>{
+    let socialFollowers = getRandomInt(0, 60000000)
+    followers+= socialFollowers
+    card.children[0].textContent = accounts[selectedAccounts][index]
+    card.children[1].textContent = formatNumber(socialFollowers)
+    card.children[3].textContent = `${formatNumber(getRandomInt(0, 1999999))} Today`
+  if (getRandomInt(0, 2) === 1)
+      card.children[3].classList.add('green')
   else
-      facebookCard.children[3].classList.add('red')
+      card.children[3].classList.add('red')
+  })
 
-  twitterCard.children[0].textContent = socialObjet['twitter-account']
-  twitterCard.children[1].textContent = socialObjet['twitter-followers']
-  twitterCard.children[3].textContent = `${socialObjet['twitter-today'].replace('+','')} Today`
-  if (socialObjet['twitter-today'].charAt(0) === '+')
-      twitterCard.children[3].classList.add('green')
-  else
-      twitterCard.children[3].classList.add('red')
+  totalFollowers.textContent = `Total Followers:  ${followers}`
 
-  instagramCard.children[0].textContent = socialObjet['instagram-account']
-  instagramCard.children[1].textContent = socialObjet['instagram-followers']
-  instagramCard.children[3].textContent = `${socialObjet['instagram-today'].replace('+','')} Today`
-  if (socialObjet['instagram-today'].charAt(0) === '+')
-      instagramCard.children[3].classList.add('green')
-  else
-      instagramCard.children[3].classList.add('red')
-
-  youtubeCard.children[0].textContent = socialObjet['youtube-account']
-  youtubeCard.children[1].textContent = socialObjet['youtube-suscribers']
-  youtubeCard.children[3].textContent = `${socialObjet['youtube-today'].replace('+','')} Today`
-  if (socialObjet['youtube-today'].charAt(0) === '+')
-      youtubeCard.children[3].classList.add('green')
-  else
-      youtubeCard.children[3].classList.add('red')
-
-  facebookPages.children[1].textContent = socialObjet['facebook-pages'][0]
-  facebookPages.children[2].textContent = `${socialObjet['facebook-pages'][1].replace('+','')}`
-  if (socialObjet['facebook-pages'][1].charAt(0) === '+')
-    facebookPages.children[2].classList.add('green')
-  else
-    facebookPages.children[2].classList.add('red')
-
-  facebookLikes.children[1].textContent = socialObjet['facebook-likes'][0]
-  facebookLikes.children[2].textContent = `${socialObjet['facebook-likes'][1].replace('+','')}`
-  if (socialObjet['facebook-likes'][1].charAt(0) === '+')
-    facebookLikes.children[2].classList.add('green')
-  else
-   facebookLikes.children[2].classList.add('red')
-
-  instagramLikes.children[1].textContent = socialObjet['instagram-likes'][0]
-  instagramLikes.children[2].textContent = `${socialObjet['instagram-likes'][1].replace('+','')}`
-  if (socialObjet['instagram-likes'][1].charAt(0) === '+')
-    instagramLikes.children[2].classList.add('green')
-  else
-    instagramLikes.children[2].classList.add('red')
-
-  instagramViews.children[1].textContent = socialObjet['instagram-views'][0]
-  instagramViews.children[2].textContent = `${socialObjet['instagram-views'][1].replace('+','')}`
-   if (socialObjet['instagram-views'][1].charAt(0) === '+')
-    instagramViews.children[2].classList.add('green')
-  else
-    instagramViews.children[2].classList.add('red')
-
-  twitterRetweets.children[1].textContent = socialObjet['twitter-retweets'][0]
-  twitterRetweets.children[2].textContent = `${socialObjet['twitter-retweets'][1].replace('+','')}`
-  if (socialObjet['twitter-retweets'][1].charAt(0) === '+')
-    twitterRetweets.children[2].classList.add('green')
-  else
-    twitterRetweets.children[2].classList.add('red')
-
-  twitterLikes.children[1].textContent = socialObjet['twitter-likes'][0]
-  twitterLikes.children[2].textContent = `${socialObjet['twitter-likes'][1].replace('+','')}`
-  if (socialObjet['twitter-likes'][1].charAt(0) === '+')
-    twitterLikes.children[2].classList.add('green')
-  else
-    twitterLikes.children[2].classList.add('red')
-
-  youtubeLikes.children[1].textContent = socialObjet['youtube-likes'][0]
-  youtubeLikes.children[2].textContent = `${socialObjet['youtube-likes'][1].replace('+','')}`
-  if (socialObjet['youtube-likes'][1].charAt(0) === '+')
-    youtubeLikes.children[2].classList.add('green')
-  else
-    youtubeLikes.children[2].classList.add('red')
-
-  youtubeViews.children[1].textContent = socialObjet['youtube-views'][0]
-  youtubeViews.children[2].textContent = `${socialObjet['youtube-views'][1].replace('+','')}`
-  if (socialObjet['youtube-views'][1].charAt(0) === '+')
-    youtubeViews.children[2].classList.add('green')
-  else
-    youtubeViews.children[2].classList.add('red')
+  overviews.forEach(overview =>{
+    overview.children[1].textContent = formatNumber(getRandomInt(0, 999999))
+    overview.children[2].textContent = `${formatNumber(getRandomInt(0, 999999))}%`
+    if  (getRandomInt(0, 2) === 1)
+      overview.children[2].classList.add('green')
+    else
+      overview.children[2].classList.add('red')
+  })
 }
 
 setData(cards[Math.floor(Math.random() * 2)])
